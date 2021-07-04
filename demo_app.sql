31631a4ce7c4da40492dc3cfdab22cb71c958647
Mehedi Hasan Shuvo <85487791+shuvo-asl@users.noreply.github.com>
2021-07-04 10:38:24 +0600

    Add Sql Backup

---
+++ demo_app.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jul 04, 2021 at 06:37 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `demo_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migration`
+--
+-- Error reading structure for table demo_app.migration: #1932 - Table 'demo_app.migration' doesn't exist in engine
+-- Error reading data for table demo_app.migration: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `demo_app`.`migration`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscriber`
+--
+-- Error reading structure for table demo_app.subscriber: #1932 - Table 'demo_app.subscriber' doesn't exist in engine
+-- Error reading data for table demo_app.subscriber: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `demo_app`.`subscriber`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table demo_app.user: #1932 - Table 'demo_app.user' doesn't exist in engine
+-- Error reading data for table demo_app.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `demo_app`.`user`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `video`
+--
+-- Error reading structure for table demo_app.video: #1932 - Table 'demo_app.video' doesn't exist in engine
+-- Error reading data for table demo_app.video: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `demo_app`.`video`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `video_like`
+--
+-- Error reading structure for table demo_app.video_like: #1932 - Table 'demo_app.video_like' doesn't exist in engine
+-- Error reading data for table demo_app.video_like: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `demo_app`.`video_like`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `video_view`
+--
+-- Error reading structure for table demo_app.video_view: #1932 - Table 'demo_app.video_view' doesn't exist in engine
+-- Error reading data for table demo_app.video_view: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `demo_app`.`video_view`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
